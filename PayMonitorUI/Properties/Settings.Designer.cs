8cef54cbe62e496a4af70ca24688927acef0f163
Roald Ignacio <roaldjap@gmail.com>
2019-02-28 12:29:04 +1300

    fixed login

--- PayMonitorUI/Properties/Settings.Designer.cs
+++ PayMonitorUI/Properties/Settings.Designer.cs
@@ -26,7 +26,7 @@ namespace PayMonitorUI.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=.;Initial Catalog=paymonitorbackup;Integrated Security=True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=.;Initial Catalog=PayMonitorDB;Integrated Security=True")]
         public string PayMonitorDB {
             get {
                 return ((string)(this["PayMonitorDB"]));
