3ab817f6c8f577994695658e303b75dea2cc5095
Roald Ignacio <roaldjap@gmail.com>
2019-03-17 19:03:37 +1300

    Done SalesReport!

---
+++ PayMonitorUI/frmSalesReport.Designer.cs
@@ -0,0 +1,329 @@
+﻿namespace PayMonitorUI
+{
+    partial class frmSalesReport
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dtEnd = new System.Windows.Forms.DateTimePicker();
+            this.dtStart = new System.Windows.Forms.DateTimePicker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblProductID = new System.Windows.Forms.Label();
+            this.lblStaff = new System.Windows.Forms.Label();
+            this.lblTransactionCode = new System.Windows.Forms.Label();
+            this.lblSalesID = new System.Windows.Forms.Label();
+            this.txtProdName = new System.Windows.Forms.TextBox();
+            this.txtProdId = new System.Windows.Forms.TextBox();
+            this.txtStaffLastName = new System.Windows.Forms.TextBox();
+            this.txtTransactionCode = new System.Windows.Forms.TextBox();
+            this.txtSalesId = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.dgSales = new System.Windows.Forms.DataGridView();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblTotalSales = new System.Windows.Forms.Label();
+            this.btnBackMainMenu = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgSales)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(23, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(171, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Sales Report";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.dtEnd);
+            this.groupBox1.Controls.Add(this.dtStart);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.lblProductID);
+            this.groupBox1.Controls.Add(this.lblStaff);
+            this.groupBox1.Controls.Add(this.lblTransactionCode);
+            this.groupBox1.Controls.Add(this.lblSalesID);
+            this.groupBox1.Controls.Add(this.txtProdName);
+            this.groupBox1.Controls.Add(this.txtProdId);
+            this.groupBox1.Controls.Add(this.txtStaffLastName);
+            this.groupBox1.Controls.Add(this.txtTransactionCode);
+            this.groupBox1.Controls.Add(this.txtSalesId);
+            this.groupBox1.Location = new System.Drawing.Point(29, 66);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(226, 394);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Filters";
+            // 
+            // dtEnd
+            // 
+            this.dtEnd.Location = new System.Drawing.Point(14, 358);
+            this.dtEnd.Name = "dtEnd";
+            this.dtEnd.Size = new System.Drawing.Size(196, 20);
+            this.dtEnd.TabIndex = 6;
+            // 
+            // dtStart
+            // 
+            this.dtStart.Location = new System.Drawing.Point(14, 311);
+            this.dtStart.Name = "dtStart";
+            this.dtStart.Size = new System.Drawing.Size(196, 20);
+            this.dtStart.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label5.Location = new System.Drawing.Point(12, 339);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 17);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "End Date";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label4.Location = new System.Drawing.Point(11, 290);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Start Date";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label3.Location = new System.Drawing.Point(11, 236);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Product Name";
+            // 
+            // lblProductID
+            // 
+            this.lblProductID.AutoSize = true;
+            this.lblProductID.BackColor = System.Drawing.Color.Transparent;
+            this.lblProductID.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblProductID.Location = new System.Drawing.Point(11, 187);
+            this.lblProductID.Name = "lblProductID";
+            this.lblProductID.Size = new System.Drawing.Size(74, 17);
+            this.lblProductID.TabIndex = 7;
+            this.lblProductID.Text = "Product ID";
+            // 
+            // lblStaff
+            // 
+            this.lblStaff.AutoSize = true;
+            this.lblStaff.BackColor = System.Drawing.Color.Transparent;
+            this.lblStaff.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblStaff.Location = new System.Drawing.Point(11, 133);
+            this.lblStaff.Name = "lblStaff";
+            this.lblStaff.Size = new System.Drawing.Size(109, 17);
+            this.lblStaff.TabIndex = 6;
+            this.lblStaff.Text = "Staff Last Name";
+            // 
+            // lblTransactionCode
+            // 
+            this.lblTransactionCode.AutoSize = true;
+            this.lblTransactionCode.BackColor = System.Drawing.Color.Transparent;
+            this.lblTransactionCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblTransactionCode.Location = new System.Drawing.Point(11, 80);
+            this.lblTransactionCode.Name = "lblTransactionCode";
+            this.lblTransactionCode.Size = new System.Drawing.Size(120, 17);
+            this.lblTransactionCode.TabIndex = 5;
+            this.lblTransactionCode.Text = "Transaction Code";
+            // 
+            // lblSalesID
+            // 
+            this.lblSalesID.AutoSize = true;
+            this.lblSalesID.BackColor = System.Drawing.Color.Transparent;
+            this.lblSalesID.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblSalesID.Location = new System.Drawing.Point(11, 28);
+            this.lblSalesID.Name = "lblSalesID";
+            this.lblSalesID.Size = new System.Drawing.Size(60, 17);
+            this.lblSalesID.TabIndex = 3;
+            this.lblSalesID.Text = "Sales ID";
+            // 
+            // txtProdName
+            // 
+            this.txtProdName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtProdName.Location = new System.Drawing.Point(11, 255);
+            this.txtProdName.Name = "txtProdName";
+            this.txtProdName.Size = new System.Drawing.Size(199, 26);
+            this.txtProdName.TabIndex = 4;
+            // 
+            // txtProdId
+            // 
+            this.txtProdId.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtProdId.Location = new System.Drawing.Point(11, 207);
+            this.txtProdId.Name = "txtProdId";
+            this.txtProdId.Size = new System.Drawing.Size(199, 26);
+            this.txtProdId.TabIndex = 3;
+            // 
+            // txtStaffLastName
+            // 
+            this.txtStaffLastName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtStaffLastName.Location = new System.Drawing.Point(11, 154);
+            this.txtStaffLastName.Name = "txtStaffLastName";
+            this.txtStaffLastName.Size = new System.Drawing.Size(199, 26);
+            this.txtStaffLastName.TabIndex = 2;
+            // 
+            // txtTransactionCode
+            // 
+            this.txtTransactionCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtTransactionCode.Location = new System.Drawing.Point(11, 100);
+            this.txtTransactionCode.Name = "txtTransactionCode";
+            this.txtTransactionCode.Size = new System.Drawing.Size(199, 26);
+            this.txtTransactionCode.TabIndex = 1;
+            // 
+            // txtSalesId
+            // 
+            this.txtSalesId.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtSalesId.Location = new System.Drawing.Point(11, 49);
+            this.txtSalesId.Name = "txtSalesId";
+            this.txtSalesId.Size = new System.Drawing.Size(199, 26);
+            this.txtSalesId.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 0;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(29, 480);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(226, 35);
+            this.btnSearch.TabIndex = 7;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // dgSales
+            // 
+            this.dgSales.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgSales.Location = new System.Drawing.Point(276, 66);
+            this.dgSales.Name = "dgSales";
+            this.dgSales.Size = new System.Drawing.Size(435, 394);
+            this.dgSales.TabIndex = 4;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(351, 484);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(187, 31);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Total Sales: $ ";
+            // 
+            // lblTotalSales
+            // 
+            this.lblTotalSales.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblTotalSales.BackColor = System.Drawing.Color.Transparent;
+            this.lblTotalSales.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalSales.Location = new System.Drawing.Point(546, 484);
+            this.lblTotalSales.Name = "lblTotalSales";
+            this.lblTotalSales.Size = new System.Drawing.Size(165, 31);
+            this.lblTotalSales.TabIndex = 0;
+            this.lblTotalSales.Text = "0";
+            this.lblTotalSales.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // btnBackMainMenu
+            // 
+            this.btnBackMainMenu.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBackMainMenu.Location = new System.Drawing.Point(512, 18);
+            this.btnBackMainMenu.Name = "btnBackMainMenu";
+            this.btnBackMainMenu.Size = new System.Drawing.Size(199, 31);
+            this.btnBackMainMenu.TabIndex = 18;
+            this.btnBackMainMenu.Text = "<< Back to Main Menu";
+            this.btnBackMainMenu.UseVisualStyleBackColor = true;
+            this.btnBackMainMenu.Click += new System.EventHandler(this.btnBackMainMenu_Click);
+            // 
+            // frmSalesReport
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(745, 536);
+            this.Controls.Add(this.btnBackMainMenu);
+            this.Controls.Add(this.lblTotalSales);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.dgSales);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "frmSalesReport";
+            this.Text = "Sales Report";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgSales)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblProductID;
+        private System.Windows.Forms.Label lblStaff;
+        private System.Windows.Forms.Label lblTransactionCode;
+        private System.Windows.Forms.Label lblSalesID;
+        private System.Windows.Forms.TextBox txtProdName;
+        private System.Windows.Forms.TextBox txtProdId;
+        private System.Windows.Forms.TextBox txtStaffLastName;
+        private System.Windows.Forms.TextBox txtTransactionCode;
+        private System.Windows.Forms.TextBox txtSalesId;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dtEnd;
+        private System.Windows.Forms.DateTimePicker dtStart;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.DataGridView dgSales;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblTotalSales;
+        private System.Windows.Forms.Button btnBackMainMenu;
+    }
+}
---
+++ PayMonitorUI/frmSalesReport.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace PayMonitorUI
+{
+    public partial class frmSalesReport : Form
+    {
+        SqlCommand cmd;
+        SqlConnection conn;
+        SqlCommand checkercmd;
+        SqlDataAdapter adapt;
+        DataTable dt;
+
+        // Initialize All Header Names
+        string sid = "Sales ID";
+        string tc = "Transaction Code";
+        string td = "Transaction Date";
+        string slname = "Staff L. Name";
+        string prodid = "Product ID";
+        string pname = "Product Name";
+        string qty = "Quantity Sold";
+        string price = "Unit Price";
+        string ttlprice = "Total Price";
+
+        string connstr = ConfigurationManager.ConnectionStrings["PayMonitorDB"].ConnectionString;
+
+        public frmSalesReport()
+        {
+            InitializeComponent();
+            Load_SalesReport();
+            
+        }
+
+        public void Load_SalesReport()
+        {
+            Load_TotalSales(); // Loads Total Sales for every
+
+            SqlDataReader datareader;
+            DateTime today = DateTime.Today;
+            string todaysql = today.ToString("yyyy-MM-dd");
+            DataTable dt = new DataTable();
+            conn = new SqlConnection(connstr);
+
+            // Rename all Headers and search all transaction within 24 hours.
+            cmd = new SqlCommand($"SELECT sales_id AS '{sid}', transaction_code  AS '{tc}', transaction_date  AS '{td}', staff_lname  AS '{slname}', product_id  AS '{prodid}', product_name  AS '{pname}', qty AS '{qty}', price AS '{price}', total_price AS '{ttlprice}' FROM tbl_sales WHERE transaction_date >= DATEADD(hh, -24, GETDATE()) ORDER BY transaction_date DESC, sales_id DESC", conn);
+
+            try
+            {
+                conn.Open();
+                datareader = cmd.ExecuteReader();
+                if (datareader.HasRows)
+                {
+                    dt.Load(datareader);
+                    dgSales.DataSource = dt;
+                }
+                else
+                {
+                    dgSales.DataSource = dt; // refresh data grid even its empty
+                    MessageBox.Show("[Checkout] : No Data to Show.");
+                }
+                datareader.Close();
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public void Load_TotalSales()
+        {
+            DataTable dt = new DataTable();
+            conn = new SqlConnection(connstr);
+            cmd = new SqlCommand("SELECT SUM(total_price) FROM tbl_sales", conn);
+
+            try
+            {
+                conn.Open();
+                // Get the sum of the table
+                Object sum = cmd.ExecuteScalar();
+                lblTotalSales.Text = sum.ToString();
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            conn = new SqlConnection(connstr);
+            string startDate = dtStart.Value.ToString("yyyy-MM-dd");
+            string endDate = dtEnd.Value.ToString("yyyy-MM-dd");
+            string sid = txtSalesId.Text;
+            string tc = txtTransactionCode.Text;
+            string sln = txtStaffLastName.Text;
+            string pid = txtProdId.Text;
+            string pname = txtProdName.Text;
+
+            // search anything by account_id, username, firstname, last_name, role_name
+            string qry = $"SELECT *, SUM(total_price) OVER () AS SumTotal FROM tbl_sales WHERE transaction_date BETWEEN '{startDate}' AND '{endDate}' OR sales_id LIKE '%{sid}%' AND transaction_code LIKE '%{tc}%' AND staff_lname LIKE '%{sln}%' AND product_id LIKE '%{pid}%' ORDER BY sales_id DESC, transaction_date DESC;";
+            adapt = new SqlDataAdapter(qry, conn);
+
+            conn.Open();
+            dt = new DataTable();
+            adapt.Fill(dt);
+            dt.Columns["sales_id"].ColumnName = "Sales ID";
+            dt.Columns["transaction_code"].ColumnName = "Transaction Code";
+            dt.Columns["transaction_date"].ColumnName = "Transaction Date";
+            dt.Columns["staff_lname"].ColumnName = "Staff L. Name";
+            dt.Columns["product_id"].ColumnName = "Product ID";
+            dt.Columns["product_name"].ColumnName = "Product Name";
+            dt.Columns["qty"].ColumnName = "Quantity";
+            dt.Columns["price"].ColumnName = "Price";
+            dt.Columns["total_price"].ColumnName = "Total Price";
+
+            Object field = dt.Rows[dt.Rows.Count - 1]["SumTotal"]; // Get Total of SumTotal
+            lblTotalSales.Text = field.ToString();
+
+            dt.Columns.RemoveAt(dt.Columns.Count - 1); // Hide Last Column after getting SumTotal
+
+            dgSales.DataSource = dt;
+            conn.Close();
+        }
+
+        private void btnBackMainMenu_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            frmMainMenu frm = new frmMainMenu();
+            frm.Show();
+        }
+    }
+}
